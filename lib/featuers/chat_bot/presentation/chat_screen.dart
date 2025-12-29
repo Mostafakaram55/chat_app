24c3ba285a63d8a1eb847c2f04bb4bb51e21ec38
ahmedmagdy99101w <ahmedmagdy99101work@gmail.com>
2025-12-30 00:08:13 +0200

    fix: add a blank line for improved code readability in ChatScreen

--- lib/featuers/chat_bot/presentation/chat_screen.dart
+++ lib/featuers/chat_bot/presentation/chat_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ChatScreen extends StatefulWidget {
+  
   const ChatScreen({super.key});
 
   @override
